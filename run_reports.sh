1d93949fc1f6768b5b18fa338d09e51a39f3811a
calvinw <calvin.e.williamson@gmail.com>
2025-01-04 08:49:01 -0500

    updating run_reports to correct names

--- run_reports.sh
+++ run_reports.sh
@@ -2,16 +2,16 @@
 
 # Add Calculated Fields (creates financial_data.csv)
 
-python3 add_calculated_financial_data.py extracted_financial_data.csv financial_data.csv
+python3 create_financial_data.py extracted_financial_data.csv financial_data.csv
 
-# Yearly Benchmark Report (creates benchmarks_structured.csv) 
+# Yearly Benchmark Report (creates benchmarks_2024.csv) 
 
-python3 create_benchmark_earnings.py -i financial_data.csv -c CIK.csv -o benchmarks_structured.csv 
+python3 create_benchmarks.py -i financial_data.csv -c CIK.csv -o benchmarks_2024.csv 
 
-# Retail Index Report - Segments (Yearly) (creates segment_analysis_output.csv)
+# Retail Index Report - Segments (Yearly) (creates segment_analysis_2024.csv)
 
-python3 segment_aggregation.py benchmarks_structured.csv segment_analysis_output.csv
+python3 create_segment_analysis.py benchmarks_2024.csv segment_analysis_2024.csv
 
-# Retail Index Report - Subsegments (Yearly) (creates subsegment_analysis_output.csv) 
+# Retail Index Report - Subsegments (Yearly) (creates subsegment_analysis_2024.csv) 
 
-python3 subsegment_aggregation.py benchmarks_structured.csv subsegment_analysis_output.csv
+python3 create_subsegment_analysis.py benchmarks_2024.csv subsegment_analysis_2024.csv

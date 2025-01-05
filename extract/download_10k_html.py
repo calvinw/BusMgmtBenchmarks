0b74937e43550037e3b87ba5b1e8174e2a8cf30c
calvinw <calvin.e.williamson@gmail.com>
2025-01-05 12:04:38 -0500

    tweaking the colab notebook

--- extract/download_10k_html.py
+++ extract/download_10k_html.py
@@ -1,1 +1,10 @@
-from edgar import set_identity, Company, Filing
+from edgar import *
+
+set_identity('calvin_williamson@fitnyc.edu')
+
+target = Company("27419")
+target_facts = target.get_facts()
+
+df = target_facts.to_pandas()
+
+print(df.head(30))
